3349a1298f40726aa4a455aacab03fc717665b04
Tom-Eric Gerritsen <ik@tom-eric.info>
2012-02-10 18:07:47 +0100

    Updated Review class for v1 API
    Updated EetNu::Review to work with the v1 API instead of the deprecated
    iPhone API.
    
    Because the new API does not have the ability to find a specific review
    or fetch a list of the latest reviews, these methods have commented out.

--- lib/eet_nu/review.rb
+++ lib/eet_nu/review.rb
@@ -6,25 +6,30 @@ module EetNu
     
     base_uri EetNu::API_URL
     
-    attr_accessor :id, :body, :ratings, :venue, :author
+                  ### Venue:
+    attr_accessor :id, :body, :rating, :scores, :author,
+
+                  ### Utility:
+                  :created_at, :updated_at
     
-    # Returns a single review specified by the id parameter
-    def self.find(id)
-      response = get "/reviews/#{id}"
-      
-      Review.new(response) if response.code == 200
-    end
-    
-    # Return the 25 latest reviews
-    def self.latest
-      response = get "/reviews"
-      response.map do |attributes|
-        Review.new(attributes)
-      end
-    end
+    # # Returns a single review specified by the id parameter
+    # def self.find(id)
+    #   response = get "/reviews/#{id}"
+    #
+    #   Review.new(response) if response.code == 200
+    # end
+    #
+    # # Return the 25 latest reviews
+    # def self.latest
+    #   response = get "/reviews"
+    #   response.map do |attributes|
+    #     Review.new(attributes)
+    #   end
+    # end
     
     def initialize(attributes = {})
       attributes.each do |key, value|
+        value.symbolize_keys! if value.respond_to?(:symbolize_keys!)
         send("#{key}=", value) if respond_to?("#{key}=")
       end
     end
--- spec/eet_nu/review_spec.rb
+++ spec/eet_nu/review_spec.rb
@@ -1,39 +1,39 @@
 require 'spec_helper'
 
 describe EetNu::Review do
-  describe '.find' do
-    context 'with a valid ID' do
-      use_vcr_cassette
-      
-      subject { EetNu::Review.find(54863) }
-      
-      it 'finds a review' do
-        subject.should be_a EetNu::Review
-      end
-      
-      it "sets the review's attributes" do
-        subject.body.should include "vriendelijkheid van het personeel"
-      end
-    end
-    
-    context 'with an invalid ID' do
-      use_vcr_cassette
-      
-      subject { EetNu::Review.find(1024 ** 4) }
-      
-      it 'returns nil' do
-        subject.should be_nil
-      end
-    end
-  end
-  
-  describe '.latest' do
-    use_vcr_cassette
-    
-    it 'finds reviews' do
-      EetNu::Review.latest[0].should be_a EetNu::Review
-    end
-  end
+  # describe '.find' do
+  #   context 'with a valid ID' do
+  #     use_vcr_cassette
+  #
+  #     subject { EetNu::Review.find(54863) }
+  #
+  #     it 'finds a review' do
+  #       subject.should be_a EetNu::Review
+  #     end
+  #
+  #     it "sets the review's attributes" do
+  #       subject.body.should include "vriendelijkheid van het personeel"
+  #     end
+  #   end
+  #
+  #   context 'with an invalid ID' do
+  #     use_vcr_cassette
+  #
+  #     subject { EetNu::Review.find(1024 ** 4) }
+  #
+  #     it 'returns nil' do
+  #       subject.should be_nil
+  #     end
+  #   end
+  # end
+  #
+  # describe '.latest' do
+  #   use_vcr_cassette
+  #
+  #   it 'finds reviews' do
+  #     EetNu::Review.latest[0].should be_a EetNu::Review
+  #   end
+  # end
   
   describe '#initialize' do
     it "sets the review's attributes" do
